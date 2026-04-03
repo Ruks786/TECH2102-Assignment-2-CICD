4ac0ee5b65509d7210f950877efa856f8ccc2392
Ruks786 <rtaiyebi@gmail.com>
2026-04-03 02:00:54 -0600

    some changes made

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,9 +3,10 @@ import { describe, test, expect } from 'vitest'
 import '@testing-library/jest-dom'
 import App from './App'
 
-describe('App', () => {
-  test('renders my name', () => {
+describe('App Component', () => {
+  test('checks if name is displayed', () => {
     render(<App />)
+    
     const text = screen.getByText(/rukaiya pardawala/i)
     expect(text).toBeInTheDocument()
   })
